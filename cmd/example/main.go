ab7a1f00d03c43617b9a7bdb792de0b5cd27e9bc
Evgen Perveev <perveev.evgen@gmail.com>
2023-03-16 08:31:08 +0200

    edit main.go file create readers outputs

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -2,26 +2,57 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	lab2 "github.com/roman-mazur/architecture-lab-2"
+	"io"
+	"os"
+	"strings"
 )
 
 var (
-	inputExpression = flag.String("e", "", "Expression to compute")
-	// TODO: Add other flags support for input and output configuration.
+  inputExpression = flag.String("e", "", "Expression to compute")
+  inputFromFile   = flag.String("f", "", "Input file")
+  resultOutput    = flag.String("o", "", "File for output")
 )
 
 func main() {
-	flag.Parse()
+  flag.Parse()
 
-	// TODO: Change this to accept input from the command line arguments as described in the task and
-	//       output the results using the ComputeHandler instance.
-	//       handler := &lab2.ComputeHandler{
-	//           Input: {construct io.Reader according the command line parameters},
-	//           Output: {construct io.Writer according the command line parameters},
-	//       }
-	//       err := handler.Compute()
+  var input io.Reader = nil
+  var output = os.Stdout
 
-	res, _ := lab2.PrefixToPostfix("+ 2 2")
-	fmt.Println(res)
+  if *inputExpression != "" {
+    input = strings.NewReader(*inputExpression)
+  }
+
+  if *inputFromFile != "" {
+    f, err := os.Open(*inputFromFile)
+    if err != nil {
+      os.Stderr.WriteString("Error opening file")
+    }
+    defer f.Close()
+    input = f
+  }
+
+  if *resultOutput != "" {
+    file, err := os.Create(*resultOutput)
+    if err != nil {
+      os.Stderr.WriteString("Error")
+    }
+    defer file.Close()
+    output = file
+  }
+
+  if input == nil {
+    os.Stderr.WriteString("No input provided")
+    return
+  }
+
+//   handler := &lab2.ComputeHandler{
+//     Input:  input,
+//     Output: output,
+//   }
+
+//   err := handler.Compute()
+//   if err != nil {
+//     println(err)
+//   }
 }
