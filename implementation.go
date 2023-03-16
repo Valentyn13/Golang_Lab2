5d0e6a2da2964fe445eea9d296d6e933001e9fbb
Valentyn13 <valentyn13git@gmail.com>
2023-03-16 08:16:09 +0200

    Create implementation of PrefixSolver function

--- implementation.go
+++ implementation.go
@@ -1,9 +1,69 @@
 package lab2
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
 
-// TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+func checkInput(str string) bool {
+	regex := regexp.MustCompile("[a-zA-Z]")
+	containsLetters := regex.MatchString(str)
+	return containsLetters
+}
+
+const ERROR_CODE float64 = 0
+
+func PrefixSolver(exprsn string) (float64, error) {
+
+	letterExist := checkInput(exprsn)
+
+	if letterExist {
+		return ERROR_CODE, fmt.Errorf("invalid expression")
+	}
+
+	if exprsn == "" {
+		return ERROR_CODE, fmt.Errorf("EOF")
+	}
+
+	stack := make([]float64, 0)
+	tokens := strings.Split(exprsn, " ")
+
+	for i := len(tokens) - 1; i >= 0; i-- {
+		token := tokens[i]
+
+		num, err := strconv.ParseFloat(token, 64)
+		if err == nil {
+			stack = append(stack, num)
+			continue
+		}
+
+		if len(stack) < 2 {
+			return ERROR_CODE, fmt.Errorf("missing operand:stack index out of range")
+		}
+		o1, o2 := stack[len(stack)-1], stack[len(stack)-2]
+		stack = stack[:len(stack)-2]
+
+		switch token {
+		case "+":
+			stack = append(stack, o1+o2)
+		case "-":
+			stack = append(stack, o1-o2)
+		case "*":
+			stack = append(stack, o1*o2)
+		case "/":
+			if o2 == 0 {
+				return ERROR_CODE, fmt.Errorf("division by zero")
+			}
+			stack = append(stack, o1/o2)
+		default:
+			return ERROR_CODE, fmt.Errorf("invalid input")
+		}
+	}
+
+	if len(stack) != 1 {
+		return ERROR_CODE, fmt.Errorf("missing operator:stack index out of range")
+	}
+	return stack[0], nil
 }
