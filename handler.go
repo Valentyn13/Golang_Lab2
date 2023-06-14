23fca5feb4bee0c11f3f9df045d63929c5c4c0fe
Valentyn13 <valentyn13git@gmail.com>
2023-06-14 17:03:36 +0300

    fix ReadAll to 10 points

--- handler.go
+++ handler.go
@@ -1,33 +1,32 @@
 package lab2
 
 import (
-	"bytes"
 	"fmt"
 	"io"
 )
 
 type ComputeHandler struct {
-  Input  io.Reader
-  Output io.Writer
+	Input  io.Reader
+	Output io.Writer
 }
 
 func (ch *ComputeHandler) Compute() error {
 
-  bufRead := make([]byte, 128)
-  _, err := ch.Input.Read(bufRead)
-  if err != nil {
-    return err
-  }
-  bufRead = bytes.Trim(bufRead, "\x00")
+	//  bufRead := make([]byte, 128)
+	// _, err := ch.Input.Read(bufRead)
+	bufRead, err := io.ReadAll(ch.Input)
+	if err != nil {
+		return err
+	}
 
-  expression := string(bufRead)
+	expression := string(bufRead)
 
-  res, err := PrefixSolver(expression)
-  if err != nil {
-    // fmt.Println("Error: ", err)
-    return err
-  }
-  resToOutput := fmt.Sprintf("%v", res)
-  ch.Output.Write([]byte(string(resToOutput)))
-  return nil
+	res, err := PrefixSolver(expression)
+	if err != nil {
+		// fmt.Println("Error: ", err)
+		return err
+	}
+	resToOutput := fmt.Sprintf("%v", res)
+	ch.Output.Write([]byte(string(resToOutput)))
+	return nil
 }
